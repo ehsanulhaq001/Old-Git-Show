6662da13b1c9ad8826a6a758deadbb79ebf901e8
Ehsan ul haq khawja <uehsan3@icloud.com>
2020-11-02 13:52:18 +0530

    Added theme toggle

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './github.jpg';
+import themeSwitch from './themeswitch.png';
 import CardList from '../Components/cardList.js';
 import Scroll from './scroll.js';
 import "./SEGA.woff";
 
 let excludeTheseRepos = ["Bounce", "ehsanulhaq001", "start-here-guidelines"];
+let preThemes = [
+  {
+    "--solid": "#ffda79",
+    "--faint": "#33d9b2",
+    "--highlight": "#ff793f"
+    },
+    {
+    "--solid": "#222f3e",
+    "--lessDark": "#334b67",
+    "--highlight": "#1ac298"
+  }
+]
+let i = 1;
 class App extends Component{
 
   constructor () {
     super();
     this.state = {
-      repos: []
+      repos: [],
+      theme: {}
     }
   }
 
@@ -29,24 +44,47 @@ class App extends Component{
     })
     .then(repos => {
       this.setState ({
-        repos: repos
-      })
-    })
+        repos: repos,
+        theme: preThemes[1]
+      });
+    });
+  }
+
+  changeTheme = () => {
+    if(i){
+      this.setState({
+        theme: preThemes[0]
+      });
+      i = 0;
+    }
+    else {
+      this.setState({
+        theme: preThemes[1]
+      });
+      i = 1;
+    }
   }
 
   render(){
+    const theme = this.state.theme;
     return (
-      <div className="bigWrapper">
+      <div className="bigWrapper"
+      style={theme}
+      >
         <header>
-        <h1>{"Ehsan Ul Haq Khawja".toUpperCase()}</h1>
-          <a href="https://github.com/ehsanulhaq001" target="_blank" rel="noopener noreferrer"><img src={logo} alt=""/></a>
+          <h1>{"Ehsan Ul Haq Khawja".toUpperCase()}</h1>
+          <div style={{"display": "flex"}}>
+            <a href="https://github.com/ehsanulhaq001" target="_blank" rel="noopener noreferrer"><img src={logo} alt=""/></a>
+            <div onClick={this.changeTheme}><img src={themeSwitch} alt=""></img></div>
+          </div>
         </header>
         <div className="main">
           <Scroll>
             <CardList repos={this.state.repos} />
           </Scroll>
         </div>
-        <div className="extra"></div>
+        <div className="extra">
+        </div>
       </div>
     );
   }
